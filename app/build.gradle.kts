d9a4f4240681d028aa31b90ee38bd39bf3b7ce0c
calvenwilliams1-pixel <291940597+calvenwilliams1-pixel@users.noreply.github.com>
2026-09-21 12:24:14 -0400

    Update build.gradle.kts
    Enable jniLibs source set in app build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,12 @@ android {
         }
     }
 
+      sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("src/main/jniLibs")
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
